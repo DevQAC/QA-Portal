6c87ea695b16a37d4e64d21c5220e3efab38dde3
jharry444 <jordan.harrison@qa.com>
2019-07-23 13:29:26 +0100

    Created structure for self-reflection-api.

---
+++ qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/SelfReflectionApplication.java
@@ -0,0 +1,14 @@
+package com.qa.portal.reflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfReflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfReflectionApplication.class, args);
+
+	}
+
+}
