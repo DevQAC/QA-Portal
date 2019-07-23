352a109cefd2c85d1327836fcfb4dabec5050864
jharry444 <jordan.harrison@qa.com>
2019-07-23 15:44:03 +0100

    Configured immutable dto's for json conversion.

--- qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/dto/QuestionDto.java
+++ qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/dto/QuestionDto.java
@@ -2,19 +2,23 @@ package com.qa.portal.reflection.dto;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.qa.portal.common.dto.QaBaseDto;
 
 public class QuestionDto extends QaBaseDto {
-	
+
 	private final Integer id;
-	
+
 	private final String body;
-	
+
 	private final String Category;
-	
+
 	private final Set<ReflectionDto> forms;
 
-	public QuestionDto(Integer id, String body, String category, Set<ReflectionDto> forms) {
+	@JsonCreator
+	public QuestionDto(@JsonProperty Integer id, @JsonProperty String body, @JsonProperty String category,
+			@JsonProperty Set<ReflectionDto> forms) {
 		super();
 		this.id = id;
 		this.body = body;
--- qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/dto/ReflectionDto.java
+++ qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/dto/ReflectionDto.java
@@ -2,20 +2,24 @@ package com.qa.portal.reflection.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.qa.portal.common.dto.QaBaseDto;
 import com.qa.portal.common.dto.QaUserDto;
 
 public final class ReflectionDto extends QaBaseDto {
-	
+
 	private final Integer id;
-	
+
 	private final QaUserDto responder;
-	
+
 	private final QaUserDto reviewer;
-	
+
 	private final LocalDate date;
 
-	public ReflectionDto(Integer id, QaUserDto responder, QaUserDto reviewer, LocalDate date) {
+	@JsonCreator
+	public ReflectionDto(@JsonProperty Integer id, @JsonProperty QaUserDto responder, @JsonProperty QaUserDto reviewer,
+			@JsonProperty LocalDate date) {
 		super();
 		this.id = id;
 		this.responder = responder;
@@ -38,7 +42,5 @@ public final class ReflectionDto extends QaBaseDto {
 	public LocalDate getDate() {
 		return date;
 	}
-	
-	
 
 }
