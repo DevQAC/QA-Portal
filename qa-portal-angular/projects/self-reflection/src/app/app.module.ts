ed575a99d38ed69f08d95424a7f81d0f1d894f2f
Scott Thomson <scottwthomson1971@gmail.com>
2019-07-23 11:02:17 +0100

    Added missing import

--- qa-portal-angular/projects/self-reflection/src/app/app.module.ts
+++ qa-portal-angular/projects/self-reflection/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {QaCommonModule} from '../../../qa-common/src/app/app.module';
 
 @NgModule({
@@ -12,20 +12,20 @@ import {QaCommonModule} from '../../../qa-common/src/app/app.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-	QaCommonModule
+    QaCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 @NgModule({})
 export class SelfReflectionSharedModule {
-static forRoot(): ModuleWithProviders {
-return {
-ngModule: AppModule,
-providers: []
-};
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AppModule,
+      providers: []
+    };
+  }
 }
-}
-	
