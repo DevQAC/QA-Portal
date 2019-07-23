459c2cc4ed544178022c47ff4794a64afd93a286
Scott Thomson <scottwthomson1971@gmail.com>
2019-07-23 11:11:45 +0100

    Removed /home from end of URLs for application table entries

--- qa-portal-infra/postgres/application-inserts.sql
+++ qa-portal-infra/postgres/application-inserts.sql
@@ -1,5 +1,5 @@
-INSERT INTO training.application (id, name, url) VALUES (5, 'Portal Home', '/qa/portal/home/view');
-INSERT INTO training.application (id, name, url) VALUES (4, 'Trainer Scheduling', '/qa/portal/training/trainer/schedule/home');
-INSERT INTO training.application (id, name, url) VALUES (1, 'Trainee Self Reflection', '/qa/portal/training/trainee/self-rating/home');
-INSERT INTO training.application (id, name, url) VALUES (2, 'Trainee Feedback', '/qa/portal/training/trainee/feedback/home');
-INSERT INTO training.application (id, name, url) VALUES (3, 'Apartment Management', '/qa/portal/hr/apartment/management/home');
+INSERT INTO training.application (id, name, url) VALUES (5, 'Portal Home', '/qa/portal');
+INSERT INTO training.application (id, name, url) VALUES (4, 'Trainer Scheduling', '/qa/portal/training/trainer/schedule');
+INSERT INTO training.application (id, name, url) VALUES (1, 'Trainee Self Reflection', '/qa/portal/training/trainee/self-rating');
+INSERT INTO training.application (id, name, url) VALUES (2, 'Trainee Feedback', '/qa/portal/training/trainee/feedback');
+INSERT INTO training.application (id, name, url) VALUES (3, 'Apartment Management', '/qa/portal/hr/apartment/management');
