e45aeed35e6dc5faf03f76921f11c83698efa87c
Fabian <frobian21@yahoo.co.uk>
2019-07-24 16:54:08 +0100

    Routing fixed. RatedQuestionComponent not working

---
+++ qa-portal-angular/projects/self-reflection/src/app/trainee-reflection/models/SelectedRatingObject.ts
@@ -0,0 +1,3 @@
+export class SelectedRatingObject implements SelectedRatingObject{
+    
+}
--- qa-portal-angular/projects/self-reflection/src/app/trainee-reflection/trainee-reflection.component.ts
+++ qa-portal-angular/projects/self-reflection/src/app/trainee-reflection/trainee-reflection.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {RatedQuestionComponent} from './../../../../qa-common/src/app/rated-question/rated-question.component';
+import {SelectedRatingModel} from './../../../../qa-common/src/app/rated-question/selected-rating.model';
+import {SelectedRatingObject} from './models/SelectedRatingObject';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-trainee-reflection',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainee-reflection.component.css']
 })
 export class TraineeReflectionComponent implements OnInit {
-
+  questions: RatedQuestionComponent;
+  rating:SelectedRatingModel;
   constructor() { }
 
   ngOnInit() {
+    // this.questions = [
+    //   {
+    //     options:10,
+    //     questionText:'hi',
+    //     selectedRating:this.ngOnInit;
+    //   }
+    // ]
   }
 
 }
