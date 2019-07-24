b1dfa79c25ec858ef955966a59a3a46c96bffb59
jharry444 <jordan.harrison@qa.com>
2019-07-24 15:37:35 +0100

    Added @Repository annotations

--- qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/repository/ReflectionRepository.java
+++ qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/repository/ReflectionRepository.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qa.portal.common.persistence.entity.TrainerEntity;
 import com.qa.portal.reflection.persistence.entity.ReflectionEntity;
 
+@Repository
 public interface ReflectionRepository extends JpaRepository<ReflectionEntity, Integer> {
 
 	Optional<ReflectionEntity> findByReviewerAndDate(TrainerEntity trainer, LocalDate date);
