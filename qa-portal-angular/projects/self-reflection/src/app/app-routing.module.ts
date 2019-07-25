9aa54eeafcac251a7ad2397887304f320e9a0720
Rameez <rameezjhaveri94@gmail.com>
2019-07-25 13:36:08 +0100

    Added component and routes

--- qa-portal-angular/projects/self-reflection/src/app/app-routing.module.ts
+++ qa-portal-angular/projects/self-reflection/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CohortSummaryComponent } from './cohort-summary/cohort-summary.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    children: [
+    {
+      path: 'cohorts', component: CohortSummaryComponent
+    }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ qa-portal-angular/projects/self-reflection/src/app/cohort-summary/cohort-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cohort-summary',
+  templateUrl: './cohort-summary.component.html',
+  styleUrls: ['./cohort-summary.component.css']
+})
+export class CohortSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
