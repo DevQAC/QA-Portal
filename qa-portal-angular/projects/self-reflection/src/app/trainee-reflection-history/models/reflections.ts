bb4d187472ff01d9aa45fdf5f2f7401b0e045208
Kiregor <kiregorekal@gmail.com>
2019-07-25 08:46:24 +0100

    Basic layout and attempt to load data from hard coded JSON

---
+++ qa-portal-angular/projects/self-reflection/src/app/trainee-reflection-history/models/reflections.ts
@@ -0,0 +1,4 @@
+export class reflections {
+  id: number;
+  date: number;
+}
--- qa-portal-angular/projects/self-reflection/src/app/trainee-reflection-history/trainee-reflection-history.component.ts
+++ qa-portal-angular/projects/self-reflection/src/app/trainee-reflection-history/trainee-reflection-history.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+export interface reflection {
+  id: number;
+  content: string;
+  date: string;
+}
+
+const reflections: reflection[] = [
+  {id:1, content:'Self Reflection Form', date: '19/07/2019'},
+  {id:2, content:'Self Reflection Form', date: '12/07/2019'}
+]
+
 @Component({
   selector: 'app-trainee-reflection-history',
   templateUrl: './trainee-reflection-history.component.html',
   styleUrls: ['./trainee-reflection-history.component.css']
 })
 export class TraineeReflectionHistoryComponent implements OnInit {
+  displayedColumns: string[] = ['content', 'date'];
+  dataSource = reflections;
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit() {
+    
   }
 
 }
