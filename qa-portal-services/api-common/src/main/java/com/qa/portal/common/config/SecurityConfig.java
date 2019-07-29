3136266ed6a57f4aab348bc37f03640cd9f3b38a
Scott Thomson <scottwthomson1971@gmail.com>
2019-07-29 12:43:19 +0100

    Added Integration tests for spring boot

--- qa-portal-services/api-common/src/main/java/com/qa/portal/common/config/SecurityConfig.java
+++ qa-portal-services/api-common/src/main/java/com/qa/portal/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.session.RegisterSessionAu
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @KeycloakConfiguration
+@Profile("!test")
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     private KeycloakConfigResolver configResolver;
