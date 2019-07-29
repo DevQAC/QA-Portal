6f590bff6e659baede6bcb02a0d2583d6ae3954e
Scott Thomson <scottwthomson1971@gmail.com>
2019-07-29 09:10:58 +0100

    Merge branch 'feature/JIRA-SR30-CreateAngularComponent' into scott-integration
    # Conflicts:
    #	qa-portal-angular/projects/self-reflection/src/app/app-routing.module.ts
    #	qa-portal-angular/projects/self-reflection/src/app/app.module.ts
    #	qa-portal-services/api-common/src/main/java/com/qa/portal/common/dto/QaUserDto.java
    #	qa-portal-services/api-common/src/main/java/com/qa/portal/common/persistence/repository/QaTraineeRepository.java
    #	qa-portal-services/api-common/src/main/java/com/qa/portal/common/persistence/repository/QaTrainerRepository.java
    #	qa-portal-services/api-common/src/main/java/com/qa/portal/common/rest/UserController.java
    #	qa-portal-services/api-common/src/main/java/com/qa/portal/common/util/mapper/BaseMapper.java
    #	qa-portal-services/self-reflection/pom.xml
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/SelfReflectionApplication.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/dto/QuestionDto.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/dto/ReflectionDto.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/entity/QuestionEntity.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/entity/ReflectionEntity.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/entity/ReflectionQuestionEntity.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/repository/ReflectionQuestionRepository.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/persistence/repository/ReflectionRepository.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/rest/ReflectionController.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/service/QuestionService.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/service/ReflectionService.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/service/mapper/QuestionMapper.java
    #	qa-portal-services/self-reflection/src/main/java/com/qa/portal/reflection/service/mapper/ReflectionMapper.java

--- qa-portal-services/core-api/src/main/resources/db/migration/V1_1__Core_DDL.sql
+++ qa-portal-services/core-api/src/main/resources/db/migration/V1_1__Core_DDL.sql
@@ -12,6 +12,22 @@ drop table if exists training.department CASCADE;
 
 drop table if exists training.application CASCADE;
 
+drop table if exists training.question CASCADE;
+
+drop table if exists training.cohort_question CASCADE;
+
+drop table if exists training.qa_user CASCADE;
+
+drop table if exists training.qa_cohort CASCADE;
+
+drop table if exists training.reflection CASCADE;
+
+drop table if exists training.reflection_question CASCADE;
+
+drop table if exists training.qa_cohort CASCADE;
+
+drop table if exists training.qa_user CASCADE;
+
 CREATE SCHEMA IF NOT EXISTS training;
 
 create schema if not exists training authorization postgres;
@@ -109,3 +125,101 @@ create table if not exists training.dept_role_menu_item
 );
 
 alter table training.dept_role_menu_item owner to postgres;
+
+create table if not exists training.qa_user
+(
+	id integer not null
+		constraint qa_user_pkey
+			primary key,
+	user_name varchar(255) not null,
+	first_name varchar(255) not null,
+	last_name varchar(255) not null,
+	reviewer_id integer
+		constraint fk_user_id
+			references training.qa_user,
+	last_updated_timestamp timestamp not null,
+	last_updated_by varchar(255) not null,
+	version integer default 1 not null,
+	role varchar(255) not null,
+	cohort_id integer
+);
+
+alter table training.qa_user owner to postgres;
+
+
+create table if not exists training.qa_cohort
+(
+	id integer not null
+		constraint qa_cohort_pk
+			primary key,
+	cohort_name varchar(255) not null,
+	trainer_id integer not null,
+	last_updated_timestamp timestamp not null,
+   	last_updated_by varchar(255) not null,
+   	version integer default 1 not null
+);
+
+alter table training.qa_cohort owner to postgres;
+
+create table if not exists training.question
+(
+	id integer not null
+		constraint question_pk
+			primary key,
+	body varchar(1000) not null,
+	category varchar(255) not null,
+	num_options integer not null,
+		last_updated_timestamp timestamp not null,
+	last_updated_by varchar(255) not null,
+	version integer default 1 not null
+);
+
+alter table training.question owner to postgres;
+
+
+create table if not exists training.cohort_question
+(
+	id integer not null
+		constraint cohort_question_pk
+			primary key,
+	cohort_id integer not null,
+	question_id integer not null,
+	last_updated_timestamp timestamp not null,
+	last_updated_by varchar(255) not null,
+	version integer default 1 not null
+);
+
+alter table training.cohort_question owner to postgres;
+
+
+create table if not exists training.reflection
+(
+	id integer not null
+		constraint reflection_pk
+			primary key,
+	reviewer_id integer,
+	responder_id integer,
+	form_date date not null,
+	last_updated_timestamp timestamp not null,
+	last_updated_by varchar(255) not null,
+	version integer default 1 not null
+);
+
+alter table training.reflection owner to postgres;
+
+
+create table if not exists training.reflection_question
+(
+	id integer not null
+		constraint reflection_question_pk
+			primary key,
+	reflection_id integer not null,
+	question_id integer not null,
+	response integer,
+	trainer_response integer,
+	last_updated_timestamp timestamp not null,
+	last_updated_by varchar(255) not null,
+	version integer default 1 not null
+);
+
+alter table training.reflection_question owner to postgres;
