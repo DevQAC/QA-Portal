78c4456ca749626430ede1c73d2c4fad81d37fdf
Scott Thomson <scottwthomson1971@gmail.com>
2019-07-29 17:56:09 +0100

    Removed System.out.println. Should only have Logger statements to output diagnostics.

--- qa-portal-services/api-common/src/main/java/com/qa/portal/common/rest/UserController.java
+++ qa-portal-services/api-common/src/main/java/com/qa/portal/common/rest/UserController.java
@@ -23,7 +23,6 @@ public class UserController {
 
 	@GetMapping("/user/getCohorts/{id}")
 	public ResponseEntity<List<QaCohortDto>> getCohortsForTrainer(@PathVariable("id") Integer id) {
-		System.out.println("-*****------*******IT WORKSSSSS*********---------");
 		return ResponseEntity.ok(this.service.getCohortsForTrainer(id));
 	}
 
