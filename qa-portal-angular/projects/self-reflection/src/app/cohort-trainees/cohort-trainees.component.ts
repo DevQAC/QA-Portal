b1848921d7372d99bd7f29b72e38e063bc0bce12
Jay <jaykonrad-lee@hotmail.com>
2019-08-01 12:06:52 +0100

    working, needs to look pretty now

--- qa-portal-angular/projects/self-reflection/src/app/cohort-trainees/cohort-trainees.component.ts
+++ qa-portal-angular/projects/self-reflection/src/app/cohort-trainees/cohort-trainees.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { QaCohortModel } from './models/qa-cohort.model';
 import { TraineeModel } from './models/trainee.model';
 import { Subscription, Observable } from 'rxjs';
 import { CohortTraineesService } from './services/cohort-trainees.service'
-import { mergeMap } from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-cohort-trainees',
@@ -12,30 +13,36 @@ import { mergeMap } from 'rxjs/operators';
   styleUrls: ['./cohort-trainees.component.css']
 })
 export class CohortTraineesComponent implements OnInit, OnDestroy {
-  singleCohort: Observable<{}>;
   cohorts: QaCohortModel[];
   trainees: TraineeModel[];
+  displayedColumns: string[] = ['name', 'id'];
+  displayedColumnsTrainees: string[] = ['id', 'userName', 'firstName', 'lastName'];
+
+
   subscriptionCohorts: Subscription;
   subscriptionTrainees: Subscription;
-  currentTraineeID = 1;
-  selectedCohort = 1;
   panelTraineesOpenState = false;
   panelCohortOpenState = true;
-  test;
+  dataSource = new MatTableDataSource<QaCohortModel>(this.cohorts);
+  dataSourceTrainees = new MatTableDataSource<TraineeModel>(this.trainees);
 
   constructor(private http: HttpClient, private cohortTraineesService: CohortTraineesService) {
   }
 
   ngOnInit() {
-    this.subscriptionCohorts = this.cohortTraineesService.getCohorts().subscribe((data) => this.cohorts = data
-    );
-    this.cohortTraineesService.getCohorts().pipe(
-      mergeMap(data => this.test=this.cohortTraineesService.getTrainees(data[0].getID()))
-    ).subscribe(data=> data);
-    this.subscriptionTrainees = this.cohortTraineesService.getTrainees(this.selectedCohort).subscribe(
-      (data) => this.trainees = data
+    this.subscriptionTrainees = this.cohortTraineesService.getTrainees(1).subscribe(
+      (data) => this.dataSourceTrainees = new MatTableDataSource<TraineeModel>(data)
     );
 
+    this.subscriptionCohorts = this.cohortTraineesService.getCohorts()
+      .subscribe((data) => this.dataSource = new MatTableDataSource<QaCohortModel>(data)
+      );
+  }
+
+  updateTraineesTable(input) {
+    this.subscriptionTrainees = this.cohortTraineesService.getTrainees(input).subscribe(
+      (data) => this.dataSourceTrainees = new MatTableDataSource<TraineeModel>(data)
+    );
   }
 
   ngOnDestroy(): void {
