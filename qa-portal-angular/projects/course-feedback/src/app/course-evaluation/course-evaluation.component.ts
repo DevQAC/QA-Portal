6f68466979885fa58398efe5fdf50f95e89042a4
HMSBennett <harrisonsbennett@gmail.com>
2019-08-07 12:42:32 +0100

    Table component fully created, and populated with dummy information

--- qa-portal-angular/projects/course-feedback/src/app/course-evaluation/course-evaluation.component.ts
+++ qa-portal-angular/projects/course-feedback/src/app/course-evaluation/course-evaluation.component.ts
@@ -12,32 +12,11 @@ export class CourseEvaluationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    PopulateTable();
     DisplayName();
-
 
     function DisplayName(){
       var name = document.getElementById('user-name');
       name.innerHTML = "Allan Smels";
-    }
-
-    function PopulateTable() {
-      //Replace with content from database
-      var numberOfRows = 2; 
-      var numberOfColumns = 6;
-  
-      var table = <HTMLTableElement>document.getElementById('evaluation-table');
-  
-      for(var i = 0; i < numberOfRows; i++){
-        var row = table.insertRow();
-        for(var j = 0; j < numberOfColumns; j++){
-          var k = j;
-          
-          var cell = row.insertCell(j);
-          cell.setAttribute('id', 'testTd');
-          cell.innerHTML = "Row: " + i + " Item: " + k;
-        }
-      }
     }
   }
 }
