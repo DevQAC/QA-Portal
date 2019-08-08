c5ce451ece35a510ebce874930bdedc425af5cd3
Scott Thomson <swttech2017@gmail.com>
2019-08-08 10:05:29 +0100

    Inserts for Evaluation form

--- qa-portal-services/core-api/src/main/resources/db/migration/V1_15__Question_Category_Inserts.sql
+++ qa-portal-services/core-api/src/main/resources/db/migration/V1_15__Question_Category_Inserts.sql
@@ -15,3 +15,46 @@ INSERT INTO training.question_category(
     last_updated_timestamp, last_updated_by, version)
 VALUES (3, 'Attitude', false, null, 1, 'RADIO_BUTTON', 'HORIZONTAL',
         current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (4, 'Evaluation Course Selection', false, null, 2, 'CHECK_BOX', 'INLINE',
+        current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (5, 'Evaluation Pre Course Experience', false, null, 2, 'RADIO_BUTTON', 'HORIZONTAL',
+        current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (6, 'Evaluation Trainer', false, null, 2, 'RADIO_BUTTON', 'HORIZONTAL',
+        current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (7, 'Evaluation Course Materials', false, null, 2, 'RADIO_BUTTON', 'HORIZONTAL',
+        current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (8, 'Evaluation Training Effectiveness', false, null, 2, 'RADIO_BUTTON', 'HORIZONTAL',
+        current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (9, 'Evaluation Recommend QA', false, null, 2, 'RADIO_BUTTON', 'HORIZONTAL',
+        current_timestamp, 'admin', 1);
+
+INSERT INTO training.question_category(
+    id, category_name, has_comment, comment_label, form_type_id, selection_type, display_direction,
+    last_updated_timestamp, last_updated_by, version)
+VALUES (10, 'Evaluation Continued Professional Development', false, null, 2, 'CHECK_BOX', 'INLINE',
+        current_timestamp, 'admin', 1);
+
--- qa-portal-services/core-api/src/main/resources/db/migration/V1_16__Question_Inserts.sql
+++ qa-portal-services/core-api/src/main/resources/db/migration/V1_16__Question_Inserts.sql
@@ -1,8 +1,98 @@
 delete from training.question;
 
-INSERT INTO training.question (id, body, category_id, last_updated_timestamp, last_updated_by, version, has_comment, comment_label, selection_options) VALUES (6, 'How ambitious have you been to work on projects out of the Academy to improve your skills?', '3', '2019-08-06 14:44:28.745289', 'admin', 1, false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
-INSERT INTO training.question (id, body, category_id, last_updated_timestamp, last_updated_by, version, has_comment, comment_label, selection_options) VALUES (5, 'How well have you managed your time this week at the Academy?', '3', '2019-08-06 14:44:28.745289', 'admin', 1, false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
-INSERT INTO training.question (id, body, category_id, last_updated_timestamp, last_updated_by, version, has_comment, comment_label, selection_options) VALUES (2, 'How well would you be able to explain the concepts you have learnt this week to a peer?', '1', '2019-08-06 14:44:28.745289', 'admin', 1, false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
-INSERT INTO training.question (id, body, category_id, last_updated_timestamp, last_updated_by, version, has_comment, comment_label, selection_options) VALUES (1, 'How well have you been able to use the technologies and tools you have learnt this week to solve a solution?', '1', '2019-08-06 14:44:28.745289', 'admin', 1, false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
-INSERT INTO training.question (id, body, category_id, last_updated_timestamp, last_updated_by, version, has_comment, comment_label, selection_options) VALUES (4, 'How well have you been able to present ideas and concepts to the group this week?', '2', '2019-08-06 14:44:28.745289', 'admin', 1, false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
-INSERT INTO training.question (id, body, category_id, last_updated_timestamp, last_updated_by, version, has_comment, comment_label, selection_options) VALUES (3, 'How well have you driven high standards through collaboration and teamwork this week?', '2', '2019-08-06 14:44:28.745289', 'admin', 1, false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                       VALUES (6, 'How ambitious have you been to work on projects out of the Academy to improve your skills?', '3',
+                              '2019-08-06 14:44:28.745289', 'admin', 1,
+                              false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                       VALUES (5, 'How well have you managed your time this week at the Academy?', '3',
+                               '2019-08-06 14:44:28.745289', 'admin', 1,
+                               false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (2, 'How well would you be able to explain the concepts you have learnt this week to a peer?', '1',
+                               '2019-08-06 14:44:28.745289', 'admin', 1,
+                                false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (1, 'How well have you been able to use the technologies and tools you have learnt this week to solve a solution?', '1',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (4, 'How well have you been able to present ideas and concepts to the group this week?', '2',
+                               '2019-08-06 14:44:28.745289', 'admin', 1,
+                               false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (3, 'How well have you driven high standards through collaboration and teamwork this week?', '2',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                false, null, '[''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (7, 'Our learning venue and facilities What was the main reason for attending your course?', '4',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                true, '', '[''To learn new skills in my current role'', ''Encouraged by my line manager'', ''To achieve the certification'', ''It is part of my overall personal development'', ''Other (please specify)'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (8, 'Why did you choose to book your training with QA?', '4',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                false, null, '[''I just heard about QA but never tried you before'', ''Someone called me from QA'', ''QA is my company''s chosen provider'', ''I saw an advert and then booked with QA'', ''Someone recommended QA to me'', ''I''ve used QA before for training'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (9, 'The pre booking process. Any required pre-course work and venue information supplied made me feel fully prepared for the course?', '5',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                true, 'Please provide any comments on your pre-course experience', '[''N/A'', ''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (10, 'The venue, classroom and technology used had a positive impact on my overall learning experience', '5',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                true, 'Please provide any comments on the learning venue, facilities and technology', '[''N/A'', ''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (11, 'The trainer''s knowledge, delivery and experience helped me to learn more effectively', '6',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                true, 'Please provide any comments you have about our trainer', '[''N/A'', ''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (12, 'The course materials had a positive impact on my learning experience', '7',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                true, 'Please provide any comments you have on your course materials', '[''N/A'', ''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+INSERT INTO training.question (id, body, category_id,
+                               last_updated_timestamp, last_updated_by, version,
+                               has_comment, comment_label, selection_options)
+                        VALUES (13, 'What I learned will have a positive impact on my job performance', '8',
+                                '2019-08-06 14:44:28.745289', 'admin', 1,
+                                true, 'Please provide any comments on your overall learning experience', '[''N/A'', ''1'', ''2'', ''3'', ''4'', ''5'', ''6'', ''7'', ''8'', ''9'', ''10'']');
+
+
+
+
+
