e58c810a0da1418718464602623559bd56f61a0d
delt0p <pjimis2@gmail.com>
2019-08-08 13:34:41 +0000

    Added nginx and jenkins deployments.

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -14,3 +14,11 @@ kubectl apply -f ../qa-portal-services/self-reflection/service.yaml
 
 kubectl apply -f ../qa-portal-angular/deployment.yaml
 kubectl apply -f ../qa-portal-angular/service.yaml
+
+kubectl apply -f ../jenkins/persistent-volume-jenkins.yaml
+kubectl apply -f ../jenkins/pod-jenkins.yaml
+kubectl apply -f ../jenkins/service-jenkins.yaml
+
+kubectl apply -f ../nginx/config-map.yaml
+kubectl apply -f ../nginx/deployment-nginx.yaml
+kubectl apply -f ../nginx/service-nginx.yaml
