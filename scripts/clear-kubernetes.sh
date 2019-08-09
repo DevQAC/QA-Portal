ac039ca15e04ffed2074fed1e872c82a082b739a
yamileon <yamileon.xfz@gmail.com>
2019-08-09 11:23:49 +0100

    merged nuke and clear scripts

--- scripts/clear-kubernetes.sh
+++ scripts/clear-kubernetes.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+kubectl delete deployments --all
+kubectl delete --all pods
+kubectl delete services --all
+
 kubectl delete pod --all
 kubectl delete service --all
 kubectl delete pvc --all
 kubectl delete configmap --all
 
+kubectl get all
