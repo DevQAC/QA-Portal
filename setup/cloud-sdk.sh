396d00b2dc9d43553d7a54923f317651da640da6
Johnny <Jonathan.Peckover@academytrainee.com>
2019-08-12 16:02:21 +0100

    setup jenkinsfile

---
+++ setup/cloud-sdk.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
