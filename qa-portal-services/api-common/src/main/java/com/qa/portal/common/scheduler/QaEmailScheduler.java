3dd78428678bd1409fb02b6e2d7fea257844b320
Harrison Bennett <harrisonsbennett@gmail.com>
2019-08-12 11:34:22 +0100

    Finished up cron emailing for cohorts

--- qa-portal-services/api-common/src/main/java/com/qa/portal/common/scheduler/QaEmailScheduler.java
+++ qa-portal-services/api-common/src/main/java/com/qa/portal/common/scheduler/QaEmailScheduler.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
+
 
 
 public class QaEmailScheduler {
@@ -28,26 +30,39 @@ public class QaEmailScheduler {
     public void sendScheduledEmails(String emailAddress, String emailSubject, String emailBody){
         //method call to send email
         qec.sendEmail(emailAddress,emailSubject,emailBody);
+
+
     }
 
 
-
+    @Scheduled(cron = "0 1 1 * * *")//cron 9am
     public List<CohortCourseRepository> checkDateAndSendEmail(Date endDate){
         List<CohortCourseEntity> ccr = cohortCourseRepository.findByEndDate(endDate);
 
+//      java.util.Date
+        java.util.Date currentDate = Calendar.getInstance().getTime();
+
+//      java.sql.Date
+        Calendar calendar = Calendar.getInstance();
+        java.sql.Date ourJavaDateObject = new java.sql.Date(calendar.getTime().getTime());
+
+        //gets date and compares them before sending the emails
         for(int i=0;i<ccr.size();i++){
-            ccr.get(i).getCohort().getTrainees().stream()
-                    .forEach(t -> sendScheduledEmails(t.getUserName(),"",""));
+            if(ccr.get(i).getEndDate() == ourJavaDateObject){
+                ccr.get(i).getCohort().getTrainees().stream()
+                        .forEach(t -> qec.sendEmail(t.getUserName(),"",""));
+            }
+
         }
 
-
-        for(int i=0;i<ccr.size();i++){
-            ccr.get(i).getCohort().getTrainees().stream()
-        .forEach(t -> System.out.println(t.getUserName()));
-        }
-//        if(ccr.contains()){
-//
+//        sample for loop to sebd email to each user
+//        for(int i=0;i<ccr.size();i++){
+//            ccr.get(i).getCohort().getTrainees().stream()
+//        .forEach(t -> System.out.println(t.getUserName()));
 //        }
+////        if(ccr.contains()){
+////
+////        }
 
         return null;
     }
