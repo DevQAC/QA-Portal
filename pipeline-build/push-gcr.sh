31e3eb11d5caff861f8b43cb656478ddcfe05663
Johnny <Jonathan.Peckover@academytrainee.com>
2019-08-13 16:01:53 +0100

    environment variables

--- pipeline-build/push-gcr.sh
+++ pipeline-build/push-gcr.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-$PROJECT=PLACEHOLDER
-
-
 docker tag core-api:latest eu.gcr.io/$PROJECT/qa-portal-services-core-api:latest
 docker tag self-reflection:latest eu.gcr.io/$PROJECT/qa-portal-services-self-reflection:latest
 docker tag user-api:latest eu.gcr.io/$PROJECT/qa-portal-services-user-api:latest
