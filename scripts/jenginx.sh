c8b7cf476212cdd7eb9780cc3d04aead327b4307
Johnny <Jonathan.Peckover@academytrainee.com>
2019-08-13 09:33:56 +0100

    typo no. 3

--- scripts/jenginx.sh
+++ scripts/jenginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl apply -f ../jenkins/persistent-volume-jenkins.yaml
-kubectl apply -f ../jenkins/service-acccount.yaml
+kubectl apply -f ../jenkins/service-account.yaml
 kubectl apply -f ../jenkins/cluster-role.yaml
 kubectl apply -f ../jenkins/cluster-role-binding.yaml
 kubectl apply -f ../jenkins/pod-jenkins.yaml
