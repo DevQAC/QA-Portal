70f04d4a04aeebc49159522996edef2f936761be
Scott Thomson <swttech2017@gmail.com>
2019-08-14 15:14:59 +0100

    Development merge

--- qa-portal-services/course-api/src/main/java/com/qa/portal/course/rest/CourseController.java
+++ qa-portal-services/course-api/src/main/java/com/qa/portal/course/rest/CourseController.java
@@ -1,8 +1,13 @@
 package com.qa.portal.course.rest;
 
+import com.qa.portal.common.dto.CourseDto;
 import com.qa.portal.common.security.QaSecurityContext;
 import com.qa.portal.course.services.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CourseController {
@@ -14,5 +19,10 @@ public class CourseController {
     public CourseController(CourseService service, QaSecurityContext qaSecurityContext) {
         this.service = service;
         this.qaSecurityContext = qaSecurityContext;
+    }
+
+    @GetMapping("courses")
+    public ResponseEntity<List<CourseDto>> getCourses() {
+        return ResponseEntity.ok(service.getCourses());
     }
 }
