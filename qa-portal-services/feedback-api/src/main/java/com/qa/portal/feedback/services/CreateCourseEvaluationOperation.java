966bde7d4ea83df1101ad9473fcc2f3cf83e762c
Scott Thomson <swttech2017@gmail.com>
2019-08-15 16:37:38 +0100

    Fix issue with mapper being incorrectly named and clashing with mapper in common project

--- qa-portal-services/feedback-api/src/main/java/com/qa/portal/feedback/services/CreateCourseEvaluationOperation.java
+++ qa-portal-services/feedback-api/src/main/java/com/qa/portal/feedback/services/CreateCourseEvaluationOperation.java
@@ -1,25 +1,25 @@
 package com.qa.portal.feedback.services;
 
 import com.qa.portal.feedback.dto.CohortCourseEvaluationDto;
-import com.qa.portal.feedback.mapper.CohortCourseMapper;
+import com.qa.portal.feedback.mapper.CohortCourseEvaluationMapper;
 import com.qa.portal.feedback.persistence.repository.CohortCourseEvaluationRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateCourseEvaluationOperation {
 	
-	private CohortCourseMapper cohortMapper;
+	private CohortCourseEvaluationMapper cohortCourseEvaluationMapper;
 	
 	private CohortCourseEvaluationRepository cohortCourseEvaluationRepository;
 
-	public CreateCourseEvaluationOperation(CohortCourseMapper cohortMapper,
+	public CreateCourseEvaluationOperation(CohortCourseEvaluationMapper cohortCourseEvaluationMapper,
 										   CohortCourseEvaluationRepository cohortCourseEvaluationRepository) {
-		this.cohortMapper = cohortMapper;
+		this.cohortCourseEvaluationMapper = cohortCourseEvaluationMapper;
 		this.cohortCourseEvaluationRepository = cohortCourseEvaluationRepository;
 	}
 
 	public CohortCourseEvaluationDto createCourseEvaluation(CohortCourseEvaluationDto courseEvaluation) {
-		return this.cohortMapper
-				.mapToQaCohortDto(this.cohortCourseEvaluationRepository.save(this.cohortMapper.mapToQaCohortEntity(courseEvaluation)));
+		return this.cohortCourseEvaluationMapper
+				.mapToQaCohortDto(this.cohortCourseEvaluationRepository.save(this.cohortCourseEvaluationMapper.mapToQaCohortEntity(courseEvaluation)));
 	}
 }
