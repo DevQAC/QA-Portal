ffb5a0cb7fd54c1aeee779d71711a0fc29eb0811
ioawnen <ioawnen@gmail.com>
2019-08-20 14:41:41 +0100

    Update header, add base custom theme

--- qa-portal-angular/projects/portal-core/src/app/portal-header/portal-header.component.ts
+++ qa-portal-angular/projects/portal-core/src/app/portal-header/portal-header.component.ts
@@ -1,33 +1,34 @@
-import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
-import {MenuService} from '../_common/services/menu.service';
-import {Subscription} from 'rxjs';
-import {ApplicationSelectionService} from '../_common/services/application-selection.service';
-import {Application} from '../_common/models/application';
-import {KeycloakService} from 'keycloak-angular';
-import {environment} from '../../environments/environment';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { MenuService } from '../_common/services/menu.service';
+import { Subscription } from 'rxjs';
+import { ApplicationSelectionService } from '../_common/services/application-selection.service';
+import { Application } from '../_common/models/application';
+import { KeycloakService } from 'keycloak-angular';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-portal-header',
-  templateUrl: './portal-header.component.html',
-  styleUrls: ['./portal-header.component.css']
+  templateUrl: './portal-header.component.html'
 })
 export class PortalHeaderComponent implements OnInit, OnDestroy {
   portalApplications: any[] = [];
+
+  displayName = 'EXAMPLE USER';
 
   navMenuSubscription: Subscription;
 
   @Output() public sidenavToggle = new EventEmitter();
 
   constructor(private menuService: MenuService,
-              private applicationSelectionService: ApplicationSelectionService,
-              private keycloak: KeycloakService) {
+    private applicationSelectionService: ApplicationSelectionService,
+    private keycloak: KeycloakService) {
   }
 
   ngOnInit() {
     this.navMenuSubscription = this.menuService.getPortalMenu()
       .subscribe((response) => {
-          this.portalApplications = response;
-        }
+        this.portalApplications = response;
+      }
       );
   }
 
