6086b28c58eb58900432b3eecd4a28256002d83b
Scott Thomson <swttech2017@gmail.com>
2019-08-22 12:46:11 +0100

    Added new tables for skills and CV statuses

---
+++ qa-portal-services/core-api/src/main/resources/db/migration/V1_23__Course_Technologies_Inserts.sql
@@ -0,0 +1,21 @@
+INSERT INTO training.course_technology(
+	id, course_id, technology_id, last_updated_timestamp, last_updated_by, version)
+	VALUES (1, 1, 1,
+	        current_timestamp, 'admin', 1);
+
+INSERT INTO training.course_technology(
+	id, course_id, technology_id, last_updated_timestamp, last_updated_by, version)
+	VALUES (2, 1, 5,
+	        current_timestamp, 'admin', 1);
+
+INSERT INTO training.course_technology(
+	id, course_id, technology_id, last_updated_timestamp, last_updated_by, version)
+	VALUES (3, 1, 8,
+	        current_timestamp, 'admin', 1);
+
+INSERT INTO training.course_technology(
+	id, course_id, technology_id, last_updated_timestamp, last_updated_by, version)
+	VALUES (4, 1, 28,
+	        current_timestamp, 'admin', 1);
+
+-- Large amount of work to create all associations.
