3887ccc3847f9fec4ffdfc1a0f73097480ac8f5c
Mohare993 <Matthew.O'Hare@academytrainee.com>
2019-08-23 10:58:20 +0100

    Qual comp wrapper and child comp Add + Delete

--- qa-portal-angular/projects/qa-cv/src/app/cv-qualis-wrapper/cv-qualis-wrapper.component.ts
+++ qa-portal-angular/projects/qa-cv/src/app/cv-qualis-wrapper/cv-qualis-wrapper.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IQualification } from '../_common/models/qualification.model';
 
 @Component({
   selector: 'app-cv-qualis-wrapper',
   templateUrl: './cv-qualis-wrapper.component.html',
   styleUrls: ['./cv-qualis-wrapper.component.css']
 })
-export class CvQualisWrapperComponent implements OnInit {
+export class CvQualisWrapperComponent {
+  @Input() qualifications: IQualification[];
+  @Output() qualificationsChange = new EventEmitter<IQualification[]>();
 
-  constructor() { }
+  onNewQualiClick(): void {
+    this.qualifications = [{
+      q_detail: '<<NEW QUALI DEFAULT>>',
+      feedback: []
+    }, ...this.qualifications];
+    this.qualificationsChange.emit(this.qualifications);
+  }
 
-  ngOnInit() {
+  onDeleteQualiEvent(index: number): void {
+    this.qualifications.splice(index, 1);
+    this.qualificationsChange.emit(this.qualifications);
   }
 
 }
--- qa-portal-angular/projects/qa-cv/src/app/cv-qualis-wrapper/cv-qualis/cv-qualis.component.ts
+++ qa-portal-angular/projects/qa-cv/src/app/cv-qualis-wrapper/cv-qualis/cv-qualis.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { IQualification } from '../../_common/models/qualification.model';
 
+
 @Component({
   selector: 'app-cv-qualis',
   templateUrl: './cv-qualis.component.html',
   styleUrls: ['./cv-qualis.component.css']
 })
 export class CvQualisComponent implements OnInit {
-  @Input() qualifications: IQualification;
+  @Input() qualifications1: IQualification;
   @Output() qualificationsChange = new EventEmitter<IQualification>();
 
+  @Output() deleteQualification = new EventEmitter<IQualification>();
+
+  
+ 
+
   constructor() { }
+
+  
+  deleteQuali(): void {
+
+    this.deleteQualification.emit(this.qualifications1);
+
+  }
 
   ngOnInit() {
   }
 
   onInputChange(data) {
-    this.qualifications.q_detail = data;
-    this.qualificationsChange.emit(this.qualifications);
+    this.qualifications1.q_detail = data;
+    this.qualificationsChange.emit(this.qualifications1);
   }
 
 }
