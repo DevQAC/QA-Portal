8391f277570fa0007e0ca7e5f1a2dd9003353e51
Akthar <akth8r@hotmail.com>
2019-08-23 10:31:39 +0100

    Created CreateCvOperation and added transactional annotation to cv management

--- qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/services/CvManagementService.java
+++ qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/services/CvManagementService.java
@@ -3,9 +3,12 @@ package com.qa.portal.cv.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CvManagementService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CvManagementService.class);
+    
 }
