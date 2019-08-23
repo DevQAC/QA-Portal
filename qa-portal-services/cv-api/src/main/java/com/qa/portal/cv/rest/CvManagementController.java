ea0640b52a284c81f04c2d4ce35b97729e8503e4
Akthar <akth8r@hotmail.com>
2019-08-23 11:49:13 +0100

    Updated cv management controller

--- qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/rest/CvManagementController.java
+++ qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/rest/CvManagementController.java
@@ -1,8 +1,27 @@
 package com.qa.portal.cv.rest;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.portal.cv.domain.CvVersion;
+import com.qa.portal.cv.services.CvManagementService;
+
 @RestController
+@RequestMapping("/cv")
 public class CvManagementController {
+	
+	private CvManagementService service;
+	
+	public CvManagementController(CvManagementService service) {
+		super();
+		this.service = service;
+	}
+	
+	@PostMapping("/create")
+	public String createCv(@RequestBody CvVersion newCv) {
+		return this.service.createCv(newCv);
+	}
 
 }
