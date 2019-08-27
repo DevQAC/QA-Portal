da2fd19e531e2d6165d4b579e780ab7c7afda298
Akthar <akth8r@hotmail.com>
2019-08-27 14:35:32 +0100

    Added UpdateCvVersionOperation class

---
+++ qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/services/UpdateCvVersionOperation.java
@@ -0,0 +1,25 @@
+package com.qa.portal.cv.services;
+
+import org.springframework.stereotype.Component;
+
+import com.qa.portal.cv.domain.CvVersion;
+import com.qa.portal.cv.persistence.repository.CvVersionRepository;
+
+@Component
+public class UpdateCvVersionOperation {
+	
+	private CvVersionRepository repo;
+	
+	public UpdateCvVersionOperation(CvVersionRepository repo) {
+		super();
+		this.repo = repo;
+	}
+
+	public String updateCv(CvVersion updatedCv) {
+		
+		repo.save(updatedCv);
+		
+		return "placeholder";
+	}
+	
+}
