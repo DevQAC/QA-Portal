a263f16b51f90867052898e0157f65bc9c62c2d6
BrotherCrayon <jake.fellows@hotmail.com>
2019-08-28 10:24:34 +0100

    added ignore case and like function to fullName search

--- qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/services/GetCurrentCvVersionOperation.java
+++ qa-portal-services/cv-api/src/main/java/com/qa/portal/cv/services/GetCurrentCvVersionOperation.java
@@ -3,7 +3,11 @@ package com.qa.portal.cv.services;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.PatternSyntaxException;
 
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 import com.qa.portal.cv.domain.CvVersion;
 import com.qa.portal.cv.persistence.repository.CvVersionRepository;
@@ -11,15 +15,17 @@ import com.qa.portal.cv.persistence.repository.CvVersionRepository;
 @Component
 public class GetCurrentCvVersionOperation {
 
+	private MongoOperations mongoOperations;
 	private CvVersionRepository repo;
 	
 	private Comparator<CvVersion> cvComparator = 
 			(cv1, cv2) -> cv1.getVersionNumber() - cv2.getVersionNumber();
 	
 	
-	public GetCurrentCvVersionOperation(CvVersionRepository repo) {
+	public GetCurrentCvVersionOperation(CvVersionRepository repo, MongoOperations mongoOperations) {
 		super();
 		this.repo = repo;
+		this.mongoOperations = mongoOperations;
 	}
 	
 	public List<CvVersion> getAll() {
@@ -27,16 +33,21 @@ public class GetCurrentCvVersionOperation {
 		return records;
 	}
 	
-	public List<CvVersion> findFullNameIgnoreCase(String fullName) {		
-		List<CvVersion> n = repo.findByFullNameIgnoreCase(fullName);
+	public List<CvVersion> findFullNameIgnoreCase(String like) {		
+//		List<CvVersion> n = repo.findByFullNameIgnoreCase(fullName);
 //		Collections.sort(n, cvComparator);
-		if(n == null) {
-			return null; //!IMPORTANT - needs an exception handler here!
-		} else {
-			return n;			
-		}
-	
+		try{
+            Query query = new Query();
+            query.addCriteria(Criteria.where("fullName").regex(toLikeRegex(like), "i"));
+            return mongoOperations.find(query, CvVersion.class);
+        } catch(PatternSyntaxException e) {
+            return Collections.emptyList();
+        }
+		
 	}
+	private String toLikeRegex(String source) {
+        return source.replaceAll("\\*", ".*");
+    }
 	
 //	public CvVersion getCurrent(Integer versionNumber) {
 //		CvVersion cv = repo.getCurrent(versionNumber);
