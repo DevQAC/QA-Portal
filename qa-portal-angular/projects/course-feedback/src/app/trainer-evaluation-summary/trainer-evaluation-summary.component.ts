980871dec54eee89da30c263820322ab1c2400f6
Scott Thomson <swttech2017@gmail.com>
2019-09-04 14:18:35 +0100

    Trainer Course Evaluation Summary - minor changes to layout.

--- qa-portal-angular/projects/course-feedback/src/app/trainer-evaluation-summary/trainer-evaluation-summary.component.ts
+++ qa-portal-angular/projects/course-feedback/src/app/trainer-evaluation-summary/trainer-evaluation-summary.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { TrainerEvaluationServicesService } from './services/trainer-evaluation-services.service';
-import { TraineeEvaluation } from './models/trainee-evaluation-data';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {TrainerEvaluationServicesService} from './services/trainer-evaluation-services.service';
+import {TraineeEvaluation} from './models/trainee-evaluation-data';
 
 @Component({
   selector: 'app-trainer-evaluation-summary',
@@ -9,25 +9,26 @@ import { TraineeEvaluation } from './models/trainee-evaluation-data';
   styleUrls: ['./trainer-evaluation-summary.component.css']
 })
 export class TrainerEvaluationSummaryComponent implements OnInit {
-  //table data
+  // table data
   dataSource: TraineeEvaluation[] = [];
-  average: number = 0;
-  tqi: number = 0;
+  average = 0;
+  tqi = 0;
 
-  //course info data
+  // course info data
   courseName: string;
   location: string;
   startDate: string;
   duration: number;
 
   constructor(private trainerEvaluationService: TrainerEvaluationServicesService,
-               private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.initalizeTable();
   }
 
-  initalizeTable() : void {
+  initalizeTable(): void {
     this.activatedRoute.paramMap.subscribe(
       (params: ParamMap) => {
         const courseId = params.get('id');
@@ -38,36 +39,39 @@ export class TrainerEvaluationSummaryComponent implements OnInit {
       });
   }
 
-  getCourseInfo(data){
-    if(data){
-      let cohortCourse = data.cohortCourse;
+  getCourseInfo(data) {
+    if (data) {
+      const cohortCourse = data.cohortCourse;
       this.average = parseInt(cohortCourse.average);
       this.tqi = parseInt(cohortCourse.tqi);
-    
+
       this.courseName = cohortCourse.course.courseName;
-      this.location = "???";
-      
+      this.location = '???';
+
       const end = new Date(cohortCourse.endDate);
       const start = new Date(cohortCourse.startDate);
       const diffTime = Math.abs(end.getTime() - start.getTime());
 
-      this.startDate = start.getDate() + "/" + start.getMonth() + "/" + start.getFullYear();
-      this.duration = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      this.startDate = start.getDate() + '/' + start.getMonth() + '/' + start.getFullYear();
+      this.duration = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
     }
   }
 
-  getTraineeEvaulations(data) : void {
-    let tempData = [];
-    for(let response of data) {
-      let evaluation: TraineeEvaluation = {traineeName: response.trainee.firstName + " " + response.trainee.lastName, knowledge: "", recommend: ""};
-      for(let catResponse of response.categoryResponses) {
-        if(catResponse.questionCategory.categoryName === "Evaluation Trainer") {
+  getTraineeEvaulations(data): void {
+    const tempData = [];
+    for (const response of data) {
+      const evaluation: TraineeEvaluation = {
+        traineeName: response.trainee.firstName + ' ' + response.trainee.lastName,
+        knowledge: '',
+        recommend: ''
+      };
+      for (const catResponse of response.categoryResponses) {
+        if (catResponse.questionCategory.categoryName === 'Evaluation Trainer') {
           evaluation.knowledge = catResponse.questionResponses[0].responseValues[0];
-        } else if(catResponse.questionCategory.categoryName === "Evaluation Recommend QA") {
+        } else if (catResponse.questionCategory.categoryName === 'Evaluation Recommend QA') {
           evaluation.recommend = catResponse.questionResponses[0].responseValues[0];
         }
-      } 
-      //this.dataSource.push(evaluation) doesn't work for... reasons
+      }
       tempData.push(evaluation);
     }
     this.dataSource = tempData;
