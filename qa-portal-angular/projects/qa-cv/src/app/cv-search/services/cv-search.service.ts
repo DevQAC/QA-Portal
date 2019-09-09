1dca9464d347c640bfea54f13171ff9f588ac2e3
ioawnen <ioawnen@gmail.com>
2019-09-09 13:29:56 +0100

    Cleanup cv-search.service

--- qa-portal-angular/projects/qa-cv/src/app/cv-search/services/cv-search.service.ts
+++ qa-portal-angular/projects/qa-cv/src/app/cv-search/services/cv-search.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from "rxjs";
+import { Observable, of } from 'rxjs';
 import { CVSearchModel } from '../models/cv-search-model';
-import { CVS } from '../models/mock-cv';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class CVSearchHistoryService {
@@ -13,11 +12,11 @@ export class CVSearchHistoryService {
     private searchUrl = 'cv-api/cv/search';  // URL to cv-api
 
     /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
     private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
 
@@ -27,56 +26,21 @@ export class CVSearchHistoryService {
             return of(result as T);
         };
     }
-    public getCVSearches(term: string, intakeChoice: string = "", techChoice: string = "", statusChoice: string = ""): Observable<CVSearchModel[]> {
-        return of(CVS.filter((cv) => {
-            let outcome = false;
-            if (cv.fullName == term) {
-                if (intakeChoice != "") {
-                    if (intakeChoice == cv.cohort) {
-                        outcome = true;
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                if (techChoice != "") {
-                    if (techChoice == cv.allSkills[0]) {
-                        outcome = true;
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                if (statusChoice != "") {
-                    if (statusChoice == cv.status) {
-                        outcome = true;
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                if (intakeChoice == "" && techChoice == "" && statusChoice == "") {
-                    outcome = true;
-                }
-            }
-            return outcome;
-        }));
-    }
 
     /* GET cvs whose name contains search term */
-    searchCVs(term: string, intakeChoice: string = "", techChoice: string = "", statusChoice: string = ""): Observable<CVSearchModel[]> {
+    searchCVs(name: string, cohort: string = '', tech: string = '', status: string = ''): Observable<CVSearchModel[]> {
         const params = new URLSearchParams();
-        if(term) {
-            params.set('name', term);
+        if (name) {
+            params.set('name', name);
         }
-        if(intakeChoice) {
-            params.set('cohort', intakeChoice);
+        if (cohort) {
+            params.set('cohort', cohort);
         }
-        if(techChoice) {
-            params.set('tech', techChoice);
+        if (tech) {
+            params.set('tech', tech);
         }
-        if(statusChoice) {
-            params.set('status', statusChoice);
+        if (status) {
+            params.set('status', status);
         }
 
         return this.http.get<CVSearchModel[]>(`${this.searchUrl}?${params.toString()}`).pipe(
