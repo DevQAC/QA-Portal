fc282c25dbbd6dda4c2333227f5a35ec509b75cc
ioawnen <ioawnen@gmail.com>
2019-09-18 09:33:30 +0100

    Cleanup data-table, documentation

--- qa-portal-angular/projects/qa-common/src/app/data-table/data-table.component.ts
+++ qa-portal-angular/projects/qa-common/src/app/data-table/data-table.component.ts
@@ -1,20 +1,45 @@
-import { Component, Input, ViewChild, AfterContentInit, ChangeDetectorRef, ContentChildren, EventEmitter, Output, ElementRef } from '@angular/core';
-import { MatTableDataSource, MatColumnDef, MatTable, MatCheckboxChange, MatCheckbox } from '@angular/material';
+import { Component, Input, ViewChild, AfterContentInit, ChangeDetectorRef, ContentChildren, EventEmitter, Output } from '@angular/core';
+import { MatTableDataSource, MatColumnDef, MatTable, MatCheckboxChange } from '@angular/material';
 import { IRowClickEvent } from './models/row-click-event';
-
 import * as _ from 'lodash';
 
-
+/**
+ * A common interactive table for displaying and interacting with data.
+ * This component is build on MatTable with additional QA-Portal features added.
+ *
+ * @example
+ * // example.component.html
+ * // Adding data table to your component template
+ * <app-data-table [displayedColumns]="columns" [dataSource]="tableDataSource">
+ *    <ng-container matColumnDef="nameColumn">
+ *        <th mat-header-cell *matHeaderCellDef>Name</th>
+ *        <td mat-cell *matCellDef="let row">{{row}}</td>
+ *    </ng-container>
+ * </app-data-table>
+ *
+ * // example.component.ts
+ * // Providing a dataSource. This is a standard MatTableDataSource object as provided by Angular Material.
+ * public tableDataSource = new MatTableDataSource<string>(['Ian', 'Scott']);
+ * // Set which columns to display with an array of column names.
+ * public columns = ['nameColumn'];
+ *
+ * @export
+ * @template DataType Type of data being provied via 'dataSource'
+ */
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html'
 })
 export class DataTableComponent<DataType> implements AfterContentInit {
+  /** Base MatTable instance */
   @ViewChild('table', { static: false }) public matTable: MatTable<DataType>;
+  /** Columns being provided with ng-content */
   @ContentChildren(MatColumnDef) public columns: MatColumnDef[];
 
   // Inputs
+  /** MatTableDataSource for the MatTable to use. This is required. */
   @Input() public dataSource: MatTableDataSource<DataType>;
+  /** Whish columns to display. Update this to dynamically change columns shown to the user. */
   @Input() public displayedColumns: string[] = [];
   @Input() public isLoading = false;
 
@@ -68,17 +93,17 @@ export class DataTableComponent<DataType> implements AfterContentInit {
   public onRowCheckboxClicked(event: MouseEvent, index: number) {
     event.preventDefault(); // Stop default checkbox behaviour
 
-    if (event.shiftKey && this.lastSelectionIndex !== null) {
+    if (event.shiftKey && this.lastSelectionIndex !== null) { // SHIFT click
       if (!event.ctrlKey) {
-        this.rowSelection = [];
+        this.rowSelection = []; // Not holding CTRL too? Clear existing slection.
       }
 
-      this.rowSelection[index] = true;
+      this.rowSelection[index] = true; // Set clicked row to true.
 
       for (const i of _.range(this.lastSelectionIndex, index)) {
-        this.rowSelection[i] = true;
+        this.rowSelection[i] = true; // Select all rows from last to current row.
       }
-    } else {
+    } else { // Normal click
       this.rowSelection[index] = this.rowSelection[index] === undefined ? true : !this.rowSelection[index];
       this.lastSelectionIndex = index;
     }
@@ -88,6 +113,7 @@ export class DataTableComponent<DataType> implements AfterContentInit {
 
   // MISC. UTIL FUNCTIONS
   public getSelectedRowsData(): DataType[] {
+    // tslint:disable-next-line: variable-name
     return this.rowSelection.map((_row, index) => this.dataSource.data[index]);
   }
 
