9a0fb18fcb85881884651d0e5deaff5930038a10
ioawnen <ioawnen@gmail.com>
2019-10-02 13:49:42 +0100

    Adds technology icons

--- qa-portal-angular/projects/qa-common/src/app/qa-common.module.ts
+++ qa-portal-angular/projects/qa-common/src/app/qa-common.module.ts
@@ -47,7 +47,9 @@ const iconsMap = [
   { name: 'qa-manage-courses', path: 'assets/icons/qa_manage_courses_48px.svg' },
   { name: 'qa-manage-users', path: 'assets/icons/qa_manage_users_48px.svg' },
   { name: 'qa-manage-roles', path: 'assets/icons/qa_manage_roles_48px.svg' },
-  { name: 'qa-roles', path: 'assets/icons/qa_roles_48px.svg' }
+  { name: 'qa-manage-technology', path: 'assets/icons/qa_manage_technology_48px.svg' },
+  { name: 'qa-role', path: 'assets/icons/qa_roles_48px.svg' },
+  { name: 'qa-technology', path: 'assets/icons/qa_technology_48px.svg' }
 ];
 
 const materialModules = [
